de363b809f575adbd542df0b0e96b9250102b91d
notacoham <alexcottam12@gmail.com>
2025-07-01 12:05:51 -0600

    add initial google api files and accept prompt from command line

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+prompt = sys.argv[1]
+
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
+prompt_tokens = response.usage_metadata.prompt_token_count
+response_tokens = response.usage_metadata.candidates_token_count
+print(response.text)
+print(f"Prompt tokens: {prompt_tokens}")
+print(f"Response tokens: {response_tokens}")
