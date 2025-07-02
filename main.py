e709276e8da170640ec901a4155bd72233cf6ded
notacoham <alexcottam12@gmail.com>
2025-07-01 22:00:29 -0600

    add ability for python scripts to be run and refactor to clean up main file

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from functions.call_function import available_functions
+from prompts import system_prompt
 
 def main():
     load_dotenv()
@@ -35,12 +37,19 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
